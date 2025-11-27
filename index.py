6c4fea21a1ca46da70fed92ccf16b1f866e8b50c
“brian <brayoire@gmail.com>
2025-11-27 09:01:10 +0300

    ireri's changes

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
-my_name= brian
+my_name= "brian"
 print("Hello, " + my_name + "!")    
 
 print("Welcome to the Python programming world.")
 print("Let's get started with some coding!")
     
+
+print("This is your first Python script.")
+print("Enjoy your coding journey!")
+print("Happy coding, " + my_name + "!")
